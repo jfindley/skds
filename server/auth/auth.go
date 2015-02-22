f2300e6584ee41521895e89e59a0152ab5183914
James Findley <james.findley@digitalwindow.com>
2015-02-22 17:14:03 +0000

    Add server auth package

---
+++ server/auth/auth.go
@@ -0,0 +1,181 @@
+// Package auth handles the server-side authentication and session
+// management.
+package auth
+
+import (
+	"crypto/rand"
+	"io"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/jfindley/skds/crypto"
+	"github.com/jfindley/skds/shared"
+)
+
+var (
+	// sessionExpiry is measured in seconds.
+	sessionExpiry = 30
+	pruneInterval = 90 * time.Second
+)
+
+type Request struct {
+	Req    *http.Request
+	Body   []byte
+	Verify bool
+}
+
+type AuthObject struct {
+	Name        string
+	UID         uint
+	GID         uint
+	Admin       bool
+	Super       bool
+	SessionKey  crypto.Binary
+	SessionTime time.Time
+}
+
+type DBCreds struct {
+	Name     string
+	Password []byte
+	UID      uint
+	GID      uint
+	Admin    bool
+}
+
+type Credentials interface {
+	Get(*shared.Config) (DBCreds, error)
+}
+
+type SessionPool struct {
+	Mu   sync.Mutex
+	Pool map[int64]*AuthObject
+}
+
+func auth(cfg *shared.Config, creds Credentials, name string, password []byte) (ok bool, a *AuthObject) {
+	a = new(AuthObject)
+
+	d, err := creds.Get(cfg)
+	if err != nil {
+		return
+	}
+
+	if d.GID == 0 || d.UID == 0 {
+		return
+	}
+
+	ok, err = crypto.PasswordVerify(password, d.Password)
+	if err != nil {
+		ok = false
+		return
+	}
+	a.Name = name
+	a.UID = d.UID
+	a.GID = d.GID
+	a.Admin = d.Admin
+
+	if a.Admin && a.GID == shared.SuperGID {
+		a.Super = true
+	}
+	return
+}
+
+func (s *SessionPool) New(cfg *shared.Config, name string, password []byte,
+	creds Credentials) (ok bool, id int64) {
+
+	ok, a := auth(cfg, creds, name, password)
+	if !ok {
+		return
+	}
+
+	id, err := s.create(a)
+	if err != nil {
+		cfg.Log(1, err)
+	}
+	s.NextKey(id)
+	return
+}
+
+func (s *SessionPool) NextKey(id int64) {
+	s.Mu.Lock()
+	defer s.Mu.Unlock()
+	if _, ok := s.Pool[id]; !ok {
+		return
+	}
+	// Chance of collision is negligable, but a collision would break the session
+	for {
+		buf := make([]byte, 32)
+		_, err := io.ReadFull(rand.Reader, buf)
+		if err != nil {
+			return
+		}
+		a := s.Pool[id]
+		if !a.SessionKey.Compare(buf) {
+			a.SessionKey = buf
+			s.Pool[id] = a
+			s.Pool[id].SessionTime = time.Now()
+			break
+		}
+	}
+	return
+}
+
+func (s *SessionPool) Validate(id int64, msgMac string, url string, message []byte) (ok bool) {
+	s.Mu.Lock()
+	defer s.Mu.Unlock()
+	if _, ok = s.Pool[id]; !ok {
+		return
+	}
+	key := s.Pool[id].SessionKey
+	ok = crypto.VerifyMAC(key, msgMac, url, message)
+	if !ok {
+		return
+	}
+	if s.expired(id) {
+		return false
+	}
+	return
+}
+
+func (s *SessionPool) Pruner() {
+	for {
+		time.Sleep(pruneInterval)
+		s.Mu.Lock()
+		for id := range s.Pool {
+			if s.expired(id) {
+				delete(s.Pool, id)
+			}
+		}
+		s.Mu.Unlock()
+	}
+}
+
+func (s *SessionPool) create(a *AuthObject) (id int64, err error) {
+	s.Mu.Lock()
+	defer s.Mu.Unlock()
+	// We do this in a loop to guarentee uniqueness
+	for {
+		id, err = crypto.RandomInt()
+		if err != nil {
+			return
+		}
+		if _, ok := s.Pool[id]; !ok {
+			if s.Pool == nil {
+				s.Pool = make(map[int64]*AuthObject)
+			}
+			s.Pool[id] = a
+			break
+		}
+	}
+	s.Pool[id].SessionTime = time.Now()
+	return
+}
+
+func (s *SessionPool) expired(id int64) bool {
+	dur := time.Now().Sub(s.Pool[id].SessionTime)
+	if dur.Seconds() >= float64(sessionExpiry) {
+		return true
+	} else {
+		return false
+	}
+}
---
+++ server/auth/auth_test.go
@@ -0,0 +1,228 @@
+package auth
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/jfindley/skds/crypto"
+	"github.com/jfindley/skds/shared"
+)
+
+var (
+	err         error
+	cfg         *shared.Config
+	validPass   []byte = []byte("This is valid")
+	invalidPass []byte = []byte("This is not valid")
+)
+
+func init() {
+	cfg = new(shared.Config)
+	cfg.Init()
+}
+
+type testcreds struct {
+	name string
+}
+
+func (t *testcreds) Get(*shared.Config) (d DBCreds, err error) {
+	switch t.name {
+	case "super":
+		d.Admin = true
+		d.GID = shared.SuperGID
+		d.Password, _ = crypto.PasswordHash(validPass)
+		d.UID = 10
+	case "admin":
+		d.Admin = true
+		d.GID = shared.DefAdminGID
+		d.Password, _ = crypto.PasswordHash(validPass)
+		d.UID = 11
+	case "client":
+		d.Admin = false
+		d.GID = shared.DefClientGID
+		d.Password, _ = crypto.PasswordHash(validPass)
+		d.UID = 12
+	default:
+		err = errors.New("Record Not Found")
+	}
+	return
+}
+
+func TestSuper(t *testing.T) {
+	c := new(testcreds)
+	c.name = "super"
+
+	ok, a := auth(cfg, c, "super", validPass)
+	if !ok {
+		t.Error("Auth failed")
+	}
+	if !a.Super {
+		t.Error("Not flagged as super")
+	}
+	if !a.Admin {
+		t.Error("Not flagged as admin")
+	}
+	if a.UID != 10 {
+		t.Error("UID incorrect")
+	}
+	if a.GID != shared.SuperGID {
+		t.Error("GID incorrect")
+	}
+
+	ok, _ = auth(cfg, c, "super", invalidPass)
+	if ok {
+		t.Error("Auth passed incorrectly")
+	}
+}
+
+func TestAdmin(t *testing.T) {
+	c := new(testcreds)
+	c.name = "admin"
+
+	ok, a := auth(cfg, c, "admin", validPass)
+	if !ok {
+		t.Error("Auth failed")
+	}
+	if a.Super {
+		t.Error("Flagged as super")
+	}
+	if !a.Admin {
+		t.Error("Not flagged as admin")
+	}
+	if a.UID != 11 {
+		t.Error("UID incorrect")
+	}
+	if a.GID != shared.DefAdminGID {
+		t.Error("GID incorrect")
+	}
+
+	ok, _ = auth(cfg, c, "admin", invalidPass)
+	if ok {
+		t.Error("Auth passed incorrectly")
+	}
+}
+
+func TestClient(t *testing.T) {
+	c := new(testcreds)
+	c.name = "client"
+
+	ok, a := auth(cfg, c, "client", validPass)
+	if !ok {
+		t.Error("Auth failed")
+	}
+	if a.Super {
+		t.Error("Flagged as super")
+	}
+	if a.Admin {
+		t.Error("Flagged as admin")
+	}
+	if a.UID != 12 {
+		t.Error("UID incorrect")
+	}
+	if a.GID != shared.DefClientGID {
+		t.Error("GID incorrect")
+	}
+
+	ok, _ = auth(cfg, c, "client", invalidPass)
+	if ok {
+		t.Error("Auth passed incorrectly")
+	}
+}
+
+func TestPool(t *testing.T) {
+	p := new(SessionPool)
+	c := new(testcreds)
+	c.name = "admin"
+
+	ok, id := p.New(cfg, "admin", validPass, c)
+	if !ok {
+		t.Error("Auth failed")
+	}
+	if id == 0 {
+		t.Error("No ID assigned")
+	}
+
+	if p.expired(id) {
+		t.Error("Session should not be marked as expired")
+	}
+
+	oldKey := p.Pool[id].SessionKey
+	oldTime := p.Pool[id].SessionTime
+
+	time.Sleep(time.Nanosecond * 5000000) // 5 ms
+	p.NextKey(id)
+	dur := p.Pool[id].SessionTime.Sub(oldTime)
+	if dur.Nanoseconds() <= 5000000 {
+		t.Error("Session time not updated")
+	}
+
+	if bytes.Compare(p.Pool[id].SessionKey, oldKey) == 0 {
+		t.Error("Session key not rotated")
+	}
+
+	sessionExpiry = 0
+
+	if !p.expired(id) {
+		t.Error("Session should be marked as expired")
+	}
+}
+
+func TestValidate(t *testing.T) {
+	sessionExpiry = 30
+
+	p := new(SessionPool)
+	c := new(testcreds)
+	c.name = "admin"
+
+	ok, id := p.New(cfg, "admin", validPass, c)
+	if !ok {
+		t.Error("Auth failed")
+	}
+
+	msg := []byte("test data")
+	fakeMsg := []byte("fake message")
+
+	mac := crypto.NewMAC(p.Pool[id].SessionKey, "/login", msg)
+	fakeMac := crypto.NewMAC(p.Pool[id].SessionKey, "/login", fakeMsg)
+
+	ok = p.Validate(id, mac, "/login", msg)
+	if !ok {
+		t.Error("Message did not validate")
+	}
+
+	ok = p.Validate(id, mac, "/login", fakeMsg)
+	if ok {
+		t.Error("Fake message validated")
+	}
+
+	ok = p.Validate(id, fakeMac, "/login", msg)
+	if ok {
+		t.Error("Fake message validated")
+	}
+
+}
+
+func TestPrune(t *testing.T) {
+	pruneInterval = 1 * time.Millisecond
+	sessionExpiry = 1
+
+	p := new(SessionPool)
+	c := new(testcreds)
+	c.name = "admin"
+
+	go p.Pruner()
+
+	ok, id := p.New(cfg, "admin", validPass, c)
+	if !ok {
+		t.Error("Auth failed")
+	}
+
+	// Enough time for the session to have expired, and the pruner to have
+	// plenty of runtime to remove it.
+	time.Sleep(1050 * time.Millisecond)
+
+	if _, ok := p.Pool[id]; ok {
+		t.Error("Expired entry in pool not pruned")
+	}
+}
