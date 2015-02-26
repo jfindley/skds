6974e1c61321571a4718a967dd645e3ac370394d
James Findley <james.findley@digitalwindow.com>
2015-02-26 21:59:12 +0000

    SessionInfo now satisfies the ClientSession interface

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -6,7 +6,6 @@ import (
 	"crypto/rand"
 	"github.com/jinzhu/gorm"
 	"io"
-	// "net/http"
 	"sync"
 	"time"
 
@@ -20,8 +19,8 @@ var (
 	pruneInterval = 90 * time.Second
 )
 
-//
-type AuthObject struct {
+// SessionInfo holds the details of the user of a session.
+type SessionInfo struct {
 	Name        string
 	UID         uint
 	GID         uint
@@ -29,9 +28,12 @@ type AuthObject struct {
 	Super       bool
 	SessionKey  crypto.Binary
 	SessionTime time.Time
+	mu          sync.Mutex
 }
 
-func (a *AuthObject) CheckACL(db gorm.DB, objects ...ACL) bool {
+// CheckACL runs the lookup function of the specified object(s) and
+// returns true if the user has access to all objects specified.
+func (a *SessionInfo) CheckACL(db gorm.DB, objects ...ACL) bool {
 	if a.Super {
 		return true
 	}
@@ -45,16 +47,41 @@ func (a *AuthObject) CheckACL(db gorm.DB, objects ...ACL) bool {
 	return ok
 }
 
-type DBCreds struct {
-	Name     string
-	Password []byte
-	UID      uint
-	GID      uint
-	Admin    bool
+// NextKey rotates the key for a session.  We do this to reduce the window during
+// which a key is used.
+func (s *SessionInfo) NextKey() crypto.Binary {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// Chance of collision is negligable, but a collision would break the session.
+	for {
+		buf := make([]byte, 32)
+		_, err := io.ReadFull(rand.Reader, buf)
+		if err != nil {
+			return nil
+		}
+		if !s.SessionKey.Compare(buf) {
+			s.SessionKey = buf
+			s.SessionTime = time.Now()
+			break
+		}
+	}
+	return s.SessionKey
 }
 
-type Credentials interface {
-	Get(*shared.Config) (DBCreds, error)
+func (s *SessionInfo) GetName() string {
+	return s.Name
+}
+
+func (s *SessionInfo) GetUID() uint {
+	return s.UID
+}
+
+func (s *SessionInfo) GetGID() uint {
+	return s.GID
+}
+
+func (s *SessionInfo) GetAdmin() bool {
+	return s.Admin
 }
 
 // ACL returns true if the UID/GID pair should be allowed access to the subject.
@@ -62,82 +89,67 @@ type ACL interface {
 	Lookup(gorm.DB, uint, uint) bool
 }
 
+// Credentials is a generic interface to return the stored credentials for a user.
+type Credentials interface {
+	GetName() string
+	GetUID() uint
+	GetGID() uint
+	GetPass() []byte
+	GetAdmin() bool
+}
+
+// Auth verifies a password avainst a credentials object.
+func Auth(creds Credentials, pass []byte) (ok bool, sess *SessionInfo) {
+	sess = new(SessionInfo)
+
+	if creds.GetUID() == 0 {
+		// User does not exist.
+		return
+	}
+
+	ok, err := crypto.PasswordVerify(pass, creds.GetPass())
+	if err != nil {
+		return
+	}
+
+	sess.Name = creds.GetName()
+	sess.UID = creds.GetUID()
+	sess.GID = creds.GetGID()
+	sess.Admin = creds.GetAdmin()
+	if sess.Admin && sess.GID == shared.SuperGID {
+		sess.Super = true
+	}
+	return
+}
+
+// SessionPool is the global pool of all sessions.
 type SessionPool struct {
-	Mu   sync.Mutex
-	Pool map[int64]*AuthObject
+	mu   sync.Mutex
+	Pool map[int64]*SessionInfo
 }
 
-func auth(cfg *shared.Config, creds Credentials, name string, password []byte) (ok bool, a *AuthObject) {
-	a = new(AuthObject)
-
-	d, err := creds.Get(cfg)
+// Add adds a session to the pool
+func (s *SessionPool) Add(sess *SessionInfo) (id int64, err error) {
+	id, err = s.create(sess)
 	if err != nil {
 		return
 	}
-
-	if d.GID == 0 || d.UID == 0 {
-		return
-	}
-
-	ok, err = crypto.PasswordVerify(password, d.Password)
-	if err != nil {
-		ok = false
-		return
-	}
-	a.Name = name
-	a.UID = d.UID
-	a.GID = d.GID
-	a.Admin = d.Admin
-
-	if a.Admin && a.GID == shared.SuperGID {
-		a.Super = true
-	}
+	s.Pool[id].NextKey()
 	return
 }
 
-func (s *SessionPool) New(cfg *shared.Config, name string, password []byte,
-	creds Credentials) (ok bool, id int64) {
-
-	ok, a := auth(cfg, creds, name, password)
-	if !ok {
-		return
-	}
-
-	id, err := s.create(a)
-	if err != nil {
-		cfg.Log(1, err)
-	}
-	s.NextKey(id)
-	return
-}
-
-func (s *SessionPool) NextKey(id int64) {
-	s.Mu.Lock()
-	defer s.Mu.Unlock()
+// Get retrieves a session to the pool
+func (s *SessionPool) Get(id int64) (sess *SessionInfo) {
 	if _, ok := s.Pool[id]; !ok {
 		return
 	}
-	// Chance of collision is negligable, but a collision would break the session
-	for {
-		buf := make([]byte, 32)
-		_, err := io.ReadFull(rand.Reader, buf)
-		if err != nil {
-			return
-		}
-		a := s.Pool[id]
-		if !a.SessionKey.Compare(buf) {
-			a.SessionKey = buf
-			s.Pool[id] = a
-			s.Pool[id].SessionTime = time.Now()
-			break
-		}
-	}
-	return
+	return s.Pool[id]
 }
 
+// Validate checks that a message belongs to a session.
 func (s *SessionPool) Validate(id int64, msgMac string, url string, message []byte) (ok bool) {
-	s.Mu.Lock()
-	defer s.Mu.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	if _, ok = s.Pool[id]; !ok {
 		return
 	}
@@ -152,22 +164,23 @@ func (s *SessionPool) Validate(id int64, msgMac string, url string, message []by
 	return
 }
 
+// Pruner is a continuous loop that removes expired sessions.
 func (s *SessionPool) Pruner() {
 	for {
 		time.Sleep(pruneInterval)
-		s.Mu.Lock()
+		s.mu.Lock()
 		for id := range s.Pool {
 			if s.expired(id) {
 				delete(s.Pool, id)
 			}
 		}
-		s.Mu.Unlock()
+		s.mu.Unlock()
 	}
 }
 
-func (s *SessionPool) create(a *AuthObject) (id int64, err error) {
-	s.Mu.Lock()
-	defer s.Mu.Unlock()
+func (s *SessionPool) create(a *SessionInfo) (id int64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	// We do this in a loop to guarentee uniqueness
 	for {
 		id, err = crypto.RandomInt()
@@ -176,7 +189,7 @@ func (s *SessionPool) create(a *AuthObject) (id int64, err error) {
 		}
 		if _, ok := s.Pool[id]; !ok {
 			if s.Pool == nil {
-				s.Pool = make(map[int64]*AuthObject)
+				s.Pool = make(map[int64]*SessionInfo)
 			}
 			s.Pool[id] = a
 			break
@@ -190,7 +203,6 @@ func (s *SessionPool) expired(id int64) bool {
 	dur := time.Now().Sub(s.Pool[id].SessionTime)
 	if dur.Seconds() >= float64(sessionExpiry) {
 		return true
-	} else {
-		return false
 	}
+	return false
 }
--- server/auth/auth_test.go
+++ server/auth/auth_test.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"bytes"
-	"errors"
 	"github.com/jinzhu/gorm"
 	"testing"
 	"time"
@@ -12,15 +11,13 @@ import (
 )
 
 var (
-	err         error
 	cfg         *shared.Config
-	validPass   []byte = []byte("This is valid")
-	invalidPass []byte = []byte("This is not valid")
+	validPass   = []byte("This is valid")
+	invalidPass = []byte("This is not valid")
 )
 
 func init() {
 	cfg = new(shared.Config)
-	cfg.Init()
 }
 
 type testAcl struct {
@@ -36,30 +33,31 @@ func (t testAcl) Lookup(db gorm.DB, uid, gid uint) bool {
 }
 
 type testCreds struct {
-	name string
+	name  string
+	uid   uint
+	gid   uint
+	pass  []byte
+	admin bool
 }
 
-func (t *testCreds) Get(*shared.Config) (d DBCreds, err error) {
-	switch t.name {
-	case "super":
-		d.Admin = true
-		d.GID = shared.SuperGID
-		d.Password, _ = crypto.PasswordHash(validPass)
-		d.UID = 10
-	case "admin":
-		d.Admin = true
-		d.GID = shared.DefAdminGID
-		d.Password, _ = crypto.PasswordHash(validPass)
-		d.UID = 11
-	case "client":
-		d.Admin = false
-		d.GID = shared.DefClientGID
-		d.Password, _ = crypto.PasswordHash(validPass)
-		d.UID = 12
-	default:
-		err = errors.New("Record Not Found")
-	}
-	return
+func (t testCreds) GetName() string {
+	return t.name
+}
+
+func (t testCreds) GetUID() uint {
+	return t.uid
+}
+
+func (t testCreds) GetGID() uint {
+	return t.gid
+}
+
+func (t testCreds) GetPass() []byte {
+	return t.pass
+}
+
+func (t testCreds) GetAdmin() bool {
+	return t.admin
 }
 
 func TestACL(t *testing.T) {
@@ -67,7 +65,7 @@ func TestACL(t *testing.T) {
 
 	good := testAcl{uid: 1, gid: 1}
 	bad := testAcl{uid: 2, gid: 2}
-	me := AuthObject{UID: 1, GID: 1}
+	me := SessionInfo{UID: 1, GID: 1}
 
 	if !me.CheckACL(db, good) {
 		t.Error("ACL should pass")
@@ -83,95 +81,61 @@ func TestACL(t *testing.T) {
 
 }
 
-func TestSuper(t *testing.T) {
-	c := new(testCreds)
-	c.name = "super"
+func TestAuth(t *testing.T) {
+	var dbUser testCreds
+	var err error
 
-	ok, a := auth(cfg, c, "super", validPass)
-	if !ok {
-		t.Error("Auth failed")
-	}
-	if !a.Super {
-		t.Error("Not flagged as super")
-	}
-	if !a.Admin {
-		t.Error("Not flagged as admin")
-	}
-	if a.UID != 10 {
-		t.Error("UID incorrect")
-	}
-	if a.GID != shared.SuperGID {
-		t.Error("GID incorrect")
+	pass := []byte("password")
+
+	dbUser.name = "admin"
+	dbUser.gid = shared.SuperGID
+	dbUser.uid = 1
+	dbUser.admin = true
+	dbUser.pass, err = crypto.PasswordHash(pass)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	ok, _ = auth(cfg, c, "super", invalidPass)
+	ok, _ := Auth(dbUser, []byte("Bad password"))
 	if ok {
-		t.Error("Auth passed incorrectly")
+		t.Error("Auth passed with bad password")
 	}
-}
 
-func TestAdmin(t *testing.T) {
-	c := new(testCreds)
-	c.name = "admin"
-
-	ok, a := auth(cfg, c, "admin", validPass)
+	ok, sess := Auth(dbUser, pass)
 	if !ok {
-		t.Error("Auth failed")
+		t.Error("Failed auth")
 	}
-	if a.Super {
-		t.Error("Flagged as super")
-	}
-	if !a.Admin {
-		t.Error("Not flagged as admin")
-	}
-	if a.UID != 11 {
-		t.Error("UID incorrect")
-	}
-	if a.GID != shared.DefAdminGID {
+
+	switch {
+	case sess.Name != dbUser.name:
+		t.Error("Name incorrect")
+	case sess.GID != dbUser.gid:
 		t.Error("GID incorrect")
-	}
-
-	ok, _ = auth(cfg, c, "admin", invalidPass)
-	if ok {
-		t.Error("Auth passed incorrectly")
-	}
-}
-
-func TestClient(t *testing.T) {
-	c := new(testCreds)
-	c.name = "client"
-
-	ok, a := auth(cfg, c, "client", validPass)
-	if !ok {
-		t.Error("Auth failed")
-	}
-	if a.Super {
-		t.Error("Flagged as super")
-	}
-	if a.Admin {
-		t.Error("Flagged as admin")
-	}
-	if a.UID != 12 {
+	case sess.UID != dbUser.uid:
 		t.Error("UID incorrect")
-	}
-	if a.GID != shared.DefClientGID {
-		t.Error("GID incorrect")
+	case !sess.Admin:
+		t.Error("Admin status incorrect")
+	case !sess.Super:
+		t.Error("Superuser status incorrect")
 	}
 
-	ok, _ = auth(cfg, c, "client", invalidPass)
+	dbUser.uid = 0
+	ok, _ = Auth(dbUser, pass)
 	if ok {
-		t.Error("Auth passed incorrectly")
+		t.Error("Auth passed with bad UID")
 	}
+
 }
 
 func TestPool(t *testing.T) {
-	p := new(SessionPool)
-	c := new(testCreds)
-	c.name = "admin"
+	var err error
 
-	ok, id := p.New(cfg, "admin", validPass, c)
-	if !ok {
-		t.Error("Auth failed")
+	p := new(SessionPool)
+	sess := new(SessionInfo)
+
+	id, err := p.Add(sess)
+	if err != nil {
+		t.Error(err)
 	}
 	if id == 0 {
 		t.Error("No ID assigned")
@@ -181,17 +145,18 @@ func TestPool(t *testing.T) {
 		t.Error("Session should not be marked as expired")
 	}
 
-	oldKey := p.Pool[id].SessionKey
-	oldTime := p.Pool[id].SessionTime
+	oldKey := p.Get(id).SessionKey
+	oldTime := p.Get(id).SessionTime
 
 	time.Sleep(time.Nanosecond * 5000000) // 5 ms
-	p.NextKey(id)
-	dur := p.Pool[id].SessionTime.Sub(oldTime)
+	newKey := p.Get(id).NextKey()
+
+	dur := p.Get(id).SessionTime.Sub(oldTime)
 	if dur.Nanoseconds() <= 5000000 {
 		t.Error("Session time not updated")
 	}
 
-	if bytes.Compare(p.Pool[id].SessionKey, oldKey) == 0 {
+	if bytes.Compare(newKey, oldKey) == 0 {
 		t.Error("Session key not rotated")
 	}
 
@@ -203,24 +168,24 @@ func TestPool(t *testing.T) {
 }
 
 func TestValidate(t *testing.T) {
+	var err error
 	sessionExpiry = 30
 
 	p := new(SessionPool)
-	c := new(testCreds)
-	c.name = "admin"
+	sess := new(SessionInfo)
 
-	ok, id := p.New(cfg, "admin", validPass, c)
-	if !ok {
-		t.Error("Auth failed")
+	id, err := p.Add(sess)
+	if err != nil {
+		t.Error(err)
 	}
 
 	msg := []byte("test data")
 	fakeMsg := []byte("fake message")
 
-	mac := crypto.NewMAC(p.Pool[id].SessionKey, "/login", msg)
-	fakeMac := crypto.NewMAC(p.Pool[id].SessionKey, "/login", fakeMsg)
+	mac := crypto.NewMAC(p.Get(id).SessionKey, "/login", msg)
+	fakeMac := crypto.NewMAC(p.Get(id).SessionKey, "/login", fakeMsg)
 
-	ok = p.Validate(id, mac, "/login", msg)
+	ok := p.Validate(id, mac, "/login", msg)
 	if !ok {
 		t.Error("Message did not validate")
 	}
@@ -238,25 +203,25 @@ func TestValidate(t *testing.T) {
 }
 
 func TestPrune(t *testing.T) {
+	var err error
 	pruneInterval = 1 * time.Millisecond
 	sessionExpiry = 1
 
 	p := new(SessionPool)
-	c := new(testCreds)
-	c.name = "admin"
+	sess := new(SessionInfo)
 
 	go p.Pruner()
 
-	ok, id := p.New(cfg, "admin", validPass, c)
-	if !ok {
-		t.Error("Auth failed")
+	id, err := p.Add(sess)
+	if err != nil {
+		t.Error(err)
 	}
 
 	// Enough time for the session to have expired, and the pruner to have
 	// plenty of runtime to remove it.
 	time.Sleep(1050 * time.Millisecond)
 
-	if _, ok := p.Pool[id]; ok {
+	if p.Get(id) != nil {
 		t.Error("Expired entry in pool not pruned")
 	}
 }
