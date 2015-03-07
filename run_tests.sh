c35b4e85fbfb55d51a67d45ccb13f6b4f2192bb5
James Findley <james.findley@digitalwindow.com>
2015-03-07 22:09:59 +0000

    Add log coverage, and improve output

--- run_tests.sh
+++ run_tests.sh
@@ -6,16 +6,32 @@ dirList=(
     "./server/db"
     "./server/functions"
     "./shared"
+    "./log"
     )
 
 runTests() {
     pushd $1
     go test
+    ret=$?
     popd
+    return $ret
 }
+
+code=0
 
 for d in "${dirList[@]}"; do
     if [[ -d $d ]]; then
         runTests "$d"
+        code=$(($code + $?))
     fi
 done
+
+echo -ne "\n\n"
+
+if [[ $code == 0 ]]; then
+    echo -e "------- ALL TESTS PASSED -------\n"
+    exit 0
+else
+    echo -e "--- ONE OR MORE TESTS FAILED ---\n"
+    exit 2
+fi
